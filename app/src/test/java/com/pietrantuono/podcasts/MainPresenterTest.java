928b8bc1d5650888f7106afa1eab6b64e9bcffc9
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-12-02 23:31:11 +0000

    Add Mockito

---
+++ app/src/test/java/com/pietrantuono/podcasts/MainPresenterTest.java
@@ -0,0 +1,26 @@
+package com.pietrantuono.podcasts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainPresenterTest {
+
+	@Test
+	public void given_foo_when_bar_then_fubar() {
+
+		/*
+		 * GIVEN
+		 */
+
+		/*
+		 * WHEN
+		 */
+
+		/*
+		 * THEN
+		 */
+	}
+
+}
