53ff96b851d6159ca33859f0ae661e1970a32481
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-12-18 00:26:24 +0000

    Happy with the toolbar effects

--- app/src/main/java/com/pietrantuono/podcasts/singlepodcast/SinglePodcastActivity.java
+++ app/src/main/java/com/pietrantuono/podcasts/singlepodcast/SinglePodcastActivity.java
@@ -3,6 +3,7 @@ package com.pietrantuono.podcasts.singlepodcast;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
@@ -10,15 +11,20 @@ import android.widget.ImageView;
 import com.pietrantuono.podcasts.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SinglePodcastActivity extends AppCompatActivity implements SinglePodcastView {
     @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.image) ImageView imageView;
+    @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
+    //@BindView(R.id.image) ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podcast);
+        ButterKnife.bind(SinglePodcastActivity.this);
         setSupportActionBar(toolbar);
+        collapsingToolbarLayout.setTitle("Ffff");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
