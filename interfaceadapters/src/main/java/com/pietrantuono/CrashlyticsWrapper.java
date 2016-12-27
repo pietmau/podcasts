5f5a2aff773251f12478f58dac58467be48f4226
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-12-27 22:11:03 +0100

    Moved Crashlytics to library

---
+++ interfaceadapters/src/main/java/com/pietrantuono/CrashlyticsWrapper.java
@@ -0,0 +1,18 @@
+package com.pietrantuono;
+
+
+import com.crashlytics.android.Crashlytics;
+
+import javax.inject.Inject;
+
+public class CrashlyticsWrapper {
+
+    @Inject
+    public CrashlyticsWrapper() {
+    }
+
+    public void logException(Throwable e){
+        Crashlytics.logException(e);
+    }
+
+}
