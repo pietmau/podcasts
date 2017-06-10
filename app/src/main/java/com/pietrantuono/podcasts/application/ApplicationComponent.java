bf4aaa3d40a5067db1b081fbbbe5126a8f02c5c8
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-06-10 12:19:26 +0100

    All modules in app component

--- app/src/main/java/com/pietrantuono/podcasts/application/ApplicationComponent.java
+++ app/src/main/java/com/pietrantuono/podcasts/application/ApplicationComponent.java
@@ -1,12 +1,23 @@
 package com.pietrantuono.podcasts.application;
 
 
+import com.pietrantuono.podcasts.addpodcast.dagger.AddPodcastModule;
+import com.pietrantuono.podcasts.addpodcast.dagger.ApiLevelCheckerlModule;
+import com.pietrantuono.podcasts.addpodcast.dagger.SearchModelsModule;
+import com.pietrantuono.podcasts.main.dagger.ImageLoaderModule;
+import com.pietrantuono.podcasts.main.dagger.MainModule;
+import com.pietrantuono.podcasts.main.dagger.TransitionsModule;
+import com.pietrantuono.podcasts.singlepodcast.dagger.SinglePodcastModule;
+import com.pietrantuono.podcasts.subscribedpodcasts.di.SubscribedPodcastModule;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules = AppModule.class)
+@Component(modules = {AppModule.class, AddPodcastModule.class, ApiLevelCheckerlModule.class,
+        SearchModelsModule.class, PlayerViewModule.class, ImageLoaderModule.class, MainModule.class,
+        TransitionsModule.class, SinglePodcastModule.class, SubscribedPodcastModule.class})
 public interface ApplicationComponent {
 
     void inject(App app);
