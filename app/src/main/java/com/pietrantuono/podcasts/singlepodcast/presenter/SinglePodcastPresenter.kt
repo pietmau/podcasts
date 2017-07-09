a0773e1c57eab31ca140e4b172bb9333842e4683
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-07-09 13:12:37 +0100

    Remove download all

--- app/src/main/java/com/pietrantuono/podcasts/singlepodcast/presenter/SinglePodcastPresenter.kt
+++ app/src/main/java/com/pietrantuono/podcasts/singlepodcast/presenter/SinglePodcastPresenter.kt
@@ -16,6 +16,7 @@ CrashlyticsWrapper, val creator: MediaSourceCreator, private val player: Player?
     companion object {
         val TAG = SinglePodcastPresenter::class.java.simpleName
     }
+
     private var view: SinglePodcastView? = null
     private var podcastFeed: PodcastFeed? = null
     private var startedWithTransition: Boolean = false
@@ -106,10 +107,6 @@ CrashlyticsWrapper, val creator: MediaSourceCreator, private val player: Player?
         when (itemId) {
             android.R.id.home -> {
                 onBackPressed()
-                return true
-            }
-            R.id.download_all -> {
-                onDownloadAllPressed()
                 return true
             }
             R.id.listen_to_all -> {
