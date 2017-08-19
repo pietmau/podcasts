b2cafbc053985529e3c7996c1d81ec5850c0cf5b
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-08-19 08:52:52 +0100

    Commit

--- app/src/main/java/com/pietrantuono/podcasts/fullscreenplay/FullscreenPlayActivity.kt
+++ app/src/main/java/com/pietrantuono/podcasts/fullscreenplay/FullscreenPlayActivity.kt
@@ -1,8 +1,10 @@
 package com.pietrantuono.podcasts.fullscreenplay
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.pietrantuono.podcasts.R
+import com.pietrantuono.podcasts.addpodcast.model.pojos.Podcast
+import com.pietrantuono.podcasts.addpodcast.singlepodcast.view.AddSinglePodcastActivity
+import com.pietrantuono.podcasts.addpodcast.singlepodcast.view.DetailActivtyBase
 import com.pietrantuono.podcasts.application.App
 import com.pietrantuono.podcasts.fullscreenplay.di.FullscreenModule
 import com.pietrantuono.podcasts.fullscreenplay.presenter.FullscreenPresenter
@@ -11,7 +13,7 @@ import com.pietrantuono.podcasts.utils.STARTED_WITH_TRANSITION
 import javax.inject.Inject
 
 
-class FullscreenPlayActivity : AppCompatActivity() {
+class FullscreenPlayActivity : DetailActivtyBase() {
     @Inject lateinit var presenter: FullscreenPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,5 +32,9 @@ class FullscreenPlayActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         presenter.onStop()
+    }
+
+    override fun getImageUrl(): String? {
+        return intent.getParcelableExtra<Podcast>(AddSinglePodcastActivity.SINGLE_PODCAST_TRACK_ID)?.artworkUrl600
     }
 }
