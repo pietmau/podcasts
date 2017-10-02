7bc0eaf6492516274ebbad4579c63433dc9e4669
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-10-02 06:25:39 +0100

    Cleanup

--- app/src/main/java/com/pietrantuono/podcasts/player/player/service/di/ServiceComponent.kt
+++ app/src/main/java/com/pietrantuono/podcasts/player/player/service/di/ServiceComponent.kt
@@ -1,12 +1,10 @@
 package com.pietrantuono.podcasts.player.player.service.di
 
 import com.pietrantuono.podcasts.player.player.service.CustomMusicService
-import com.pietrantuono.podcasts.player.player.service.PlayerService
 import dagger.Subcomponent
 
 @Subcomponent(modules = arrayOf(ServiceModule::class))
 interface ServiceComponent {
 
-    fun inject(playerService: PlayerService)
     fun inject(playerService: CustomMusicService)
 }
