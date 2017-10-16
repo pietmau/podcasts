6f15871a523e6f6a0ceb977a272cf43519d7b80d
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-10-16 06:36:26 +0100

    WIP

--- app/src/main/java/com/pietrantuono/podcasts/repository/RepoServicesImpl.kt
+++ app/src/main/java/com/pietrantuono/podcasts/repository/RepoServicesImpl.kt
@@ -5,11 +5,9 @@ import android.content.Intent
 import com.pietrantuono.podcasts.addpodcast.model.pojos.Podcast
 import com.pietrantuono.podcasts.application.DebugLogger
 import com.pietrantuono.podcasts.repository.repository.RepoServices
-import io.realm.Realm
 
 class RepoServicesImpl(
         private val context: Context,
-        private val realm: Realm,
         private val logger: DebugLogger) : RepoServices {
 
     companion object {
--- app/src/main/java/com/pietrantuono/podcasts/repository/repository/PodcastRepoRealm.kt
+++ app/src/main/java/com/pietrantuono/podcasts/repository/repository/PodcastRepoRealm.kt
@@ -8,11 +8,11 @@ import rx.Observable
 import rx.Observer
 import rx.subjects.BehaviorSubject
 
-class PodcastRepoRealm(private val realm: Realm, private val reposServices: RepoServices) : PodcastRepo {
+class PodcastRepoRealm(Realm, private val reposServices: RepoServices) : PodcastRepo {
     private var subject: BehaviorSubject<Boolean>? = null
 
     override fun subscribeUnsubscribeToPodcast(podcast: Podcast?) {
-        realm.executeTransactionAsync {
+        Realm.getDefaultInstance().executeTransactionAsync {
             var singlePodcast = it
                     .where(PodcastRealm::class.java)
                     .equalTo("trackId", podcast?.trackId)
@@ -27,35 +27,41 @@ class PodcastRepoRealm(private val realm: Realm, private val reposServices: Repo
 
     override fun getIfSubscribed(podcast: Podcast?): Observable<Boolean> {
         subject = BehaviorSubject.create<Boolean>()
-        realm.where(PodcastRealm::class.java)
-                .equalTo("trackId", podcast?.trackId)
-                .findFirstAsync()
-                .asObservable<PodcastRealm>()
-                .filter { it.isLoaded && it.isValid }
-                .map { it.isPodcastSubscribed }
-                .subscribe(subject)
+        Realm.getDefaultInstance().use { realm ->
+            realm.where(PodcastRealm::class.java)
+                    .equalTo("trackId", podcast?.trackId)
+                    .findFirstAsync()
+                    .asObservable<PodcastRealm>()
+                    .filter { it.isLoaded && it.isValid }
+                    .map { it.isPodcastSubscribed }
+                    .subscribe(subject)
+        }
         return subject!!.asObservable()
     }
 
     override fun getSubscribedPodcasts(observer: Observer<List<Podcast>>): Observable<List<Podcast>> {
-        return realm.where(PodcastRealm::class.java)
-                .equalTo("podcastSubscribed", true)
-                .findAllAsync()
-                .asObservable()
-                .filter { it.isLoaded && it.isValid }
-                .map { realm.copyFromRealm(it) }
-                .map { it as List<Podcast> }
-                .cache()
+        return Realm.getDefaultInstance().use { realm ->
+            realm.where(PodcastRealm::class.java)
+                    .equalTo("podcastSubscribed", true)
+                    .findAllAsync()
+                    .asObservable()
+                    .filter { it.isLoaded && it.isValid }
+                    .map { realm.copyFromRealm(it) }
+                    .map { it as List<Podcast> }
+                    .cache()
+        }
     }
 
     override fun getPodcastById(trackId: Int): Observable<out Podcast> {
-        return realm.where(PodcastRealm::class.java)
-                .equalTo("trackId", trackId)
-                .findFirstAsync()
-                .asObservable<PodcastRealm>()
-                .filter(PodcastRealm::isLoaded)
-                .map(realm::copyFromRealm)
-                .cache()
+        return Realm.getDefaultInstance().use { realm ->
+            realm.where(PodcastRealm::class.java)
+                    .equalTo("trackId", trackId)
+                    .findFirstAsync()
+                    .asObservable<PodcastRealm>()
+                    .filter(PodcastRealm::isLoaded)
+                    .map(realm::copyFromRealm)
+                    .cache()
+        }
     }
 }
 
