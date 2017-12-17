18b181a3d3b49e19d692d92c4070661b912bd3e9
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2017-12-17 12:46:31 +0000

    Seems to be a solution

--- app/src/main/java/com/pietrantuono/podcasts/downloadfragment/model/DownloadFragmentModelImpl.kt
+++ app/src/main/java/com/pietrantuono/podcasts/downloadfragment/model/DownloadFragmentModelImpl.kt
@@ -4,13 +4,16 @@ import com.pietrantuono.podcasts.addpodcast.singlepodcast.viewmodel.ResourcesPro
 import com.pietrantuono.podcasts.application.DebugLogger
 import com.pietrantuono.podcasts.downloadfragment.view.custom.DownloadedEpisode
 import com.pietrantuono.podcasts.downloadfragment.view.custom.DownloadedPodcast
+import io.fabric.sdk.android.services.concurrency.AsyncTask
+import io.realm.Realm
 import models.pojos.Episode
 import models.pojos.Podcast
+import models.pojos.PodcastRealm
 import repo.repository.EpisodesRepository
 import repo.repository.PodcastRepo
-import rx.Observable
 import rx.Observer
 import rx.Scheduler
+import rx.subjects.PublishSubject
 import rx.subscriptions.CompositeSubscription
 
 class DownloadFragmentModelImpl(
@@ -23,29 +26,52 @@ class DownloadFragmentModelImpl(
 
     private val TAG = "DownloadFragmentModelImpl"
 
-    private var compositeSubscription: CompositeSubscription = CompositeSubscription()
+    private val compositeSubscription: CompositeSubscription = CompositeSubscription()
+
+    private val subject = PublishSubject.create<List<DownloadedPodcast>>()
 
     override fun unsubscribe() {
         compositeSubscription.unsubscribe()
     }
 
     override fun subscribe(observer: Observer<List<DownloadedPodcast>>) {
-        val subscription =
-                podcastRepo
-                        .getSubscribedPodcastsAsObservable()
-                        .subscribeOn(mainThreadScheduler)
-                        .observeOn(workerScheduler)
-                        .flatMap { list ->
-                            Observable.from(list)
-                                    .map { toDownloaded(it) }
-                                    .toList()
-                        }
-                        .observeOn(mainThreadScheduler)
-                        .subscribe(observer)
-        compositeSubscription.add(subscription)
+        diocan(observer)
     }
 
-    fun toDownloadedEpisode(episode: Episode): DownloadedEpisode = DownloadedEpisode.fromEpisode(episode, resources)
+    private fun diocan(observer: Observer<List<DownloadedPodcast>>) {
+        subject.subscribe(observer)
+        Realm.getDefaultInstance()
+                .where(PodcastRealm::class.java)
+                .equalTo("podcastSubscribed", true)
+                .findAllAsync()
+                .asObservable()
+                .subscribe { DiocanTasx(resources, subject).execute() }
+    }
+
+    class DiocanTasx(
+            private val resources: ResourcesProvider,
+            private val subject: PublishSubject<List<DownloadedPodcast>>)
+        : AsyncTask<Void, List<DownloadedPodcast>, List<DownloadedPodcast>>() {
+
+        override fun doInBackground(vararg p0: Void?): List<DownloadedPodcast> {
+            val result = Realm.getDefaultInstance().where(PodcastRealm::class.java)
+                    .equalTo("podcastSubscribed", true)
+                    .findAll()
+                    .toList()
+                    .map { toDownloaded(it as Podcast) }
+            return result
+        }
+
+        override fun onPostExecute(result: List<DownloadedPodcast>?) {
+            subject.onNext(result)
+        }
+
+        fun toDownloaded(podcast: Podcast): DownloadedPodcast = DownloadedPodcast.fromPodcast(podcast, podcast.trackName, makeEpisodes(podcast), resources)
+
+        private fun makeEpisodes(podcast: Podcast): List<DownloadedEpisode>? = podcast.episodes?.map { toDownloadedEpisode(it) }
+
+        fun toDownloadedEpisode(episode: Episode): DownloadedEpisode = DownloadedEpisode.fromEpisode(episode, resources)
+    }
 
     override fun getPodcastTitleAsync(observer: Observer<String?>, trackId: Int?) {
         trackId?.let {
@@ -56,9 +82,4 @@ class DownloadFragmentModelImpl(
                     .subscribe(observer)
         }
     }
-
-    fun toDownloaded(podcast: Podcast): DownloadedPodcast = DownloadedPodcast.fromPodcast(podcast, podcast.trackName, makeEpisodes(podcast), resources)
-
-    private fun makeEpisodes(podcast: Podcast): List<DownloadedEpisode>? = podcast.episodes?.map { toDownloadedEpisode(it) }
-
 }
--- mobile/src/main/java/repo/repository/PodcastRepoRealm.kt
+++ mobile/src/main/java/repo/repository/PodcastRepoRealm.kt
@@ -1,5 +1,6 @@
 package repo.repository
 
+import android.util.Log
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import models.pojos.DataRealmLibraryModule
@@ -96,15 +97,35 @@ class PodcastRepoRealm(
     }
 
     override fun getSubscribedPodcastsAsObservable(): Observable<List<Podcast>> {
-        return Realm.getDefaultInstance().use { realm ->
+        Log.e("DIOCAN", "getSubscribedPodcastsAsObservable ENTER " + System.currentTimeMillis())
+        val result = Realm.getDefaultInstance().use { realm ->
+            realm.where(PodcastRealm::class.java)
+                    .equalTo("podcastSubscribed", true)
+                    .findAllAsync()
+                    .asObservable()
+                    .doOnNext { Log.e("DIOCAN", "asObservable  " + System.currentTimeMillis()) }
+                    .map { realm.copyFromRealm(it) }
+                    .doOnNext { Log.e("DIOCAN", ".map { realm.copyFromRealm(it) }  " + System.currentTimeMillis()) }
+                    .map { it as List<Podcast> }
+                    .doOnNext { Log.e("DIOCAN", ".map { it as List<Podcast> }  " + System.currentTimeMillis()) }
+        }
+        Log.e("DIOCAN", "getSubscribedPodcastsAsObservable EXIT " + System.currentTimeMillis())
+        return result
+    }
+
+    fun getSubscribodcastsAsObservable(): Observable<List<Podcast>> {
+        val result = Realm.getDefaultInstance().use { realm ->
             realm.where(PodcastRealm::class.java)
                     .equalTo("podcastSubscribed", true)
                     .findAllAsync()
                     .asObservable()
                     .map { realm.copyFromRealm(it) }
                     .map { it as List<Podcast> }
+
         }
+        return result
     }
+
 
     override fun getPodcastByIdAsync(trackId: Int): Observable<out Podcast> {
         return Realm.getDefaultInstance().use { realm ->
